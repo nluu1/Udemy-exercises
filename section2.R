902d75dc0d9dd1586a4ab267b9c296e8b8abca11
nluu1 <nluu1@umbc.edu>
2022-05-01 23:59:51 -0400

    first commit

---
+++ section2.R
@@ -0,0 +1,72 @@
+### Part 1: uncomment to view
+# #interger
+# #by default, number will be stored as double 
+# #add a capital L to tell R the data is interger
+# x <- 2L
+# typeof(x)
+# 
+# #double
+# y <- 2.5
+# typeof(y)
+# 
+# #complex
+# z <-  3 + 2i
+# typeof(z)
+# 
+# #character
+# a <-  "h"
+# typeof(a)
+# 
+# #logical
+# q1 <-  T
+# typeof(q1)
+# q2 <- FALSE
+# typeof(q2)
+
+#############################
+
+# ##Part 2: uncomment to view
+# 
+# A <-  10
+# B <-  5
+# C <-  A + B
+# C
+# 
+# var1 <- 2.5
+# var2 <- 4
+# 
+# result <-  var1/var2
+# result
+# 
+# answer <- sqrt(var2)
+# answer
+# 
+# greeting <-  "Hello"
+# name <- "Nhi"
+# message <- paste(greeting,name)
+# message
+
+#############################
+
+# ##Part 3: Logical operators
+# 4 < 5
+# 10 > 100
+# 4 == 5
+# 4 != 5
+# # / means "OR"
+# # ! means "NOT"
+# # isTRUE(x)
+# 
+# result = 4 < 5
+# isTRUE(result)
+# result2 <- !TRUE
+# #or result2 <- !(5 > 1)
+# result2
+# 
+# result | result2
+# result & result2
+
+#############################
+
+# ##Part 4: While loop
+
