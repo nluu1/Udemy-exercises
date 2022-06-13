d57a87906940a01a2a51400651cb565cd8523e77
Nhi Luu <98994882+nluu1@users.noreply.github.com>
2022-06-13 19:56:14 -0400

    updated

--- section5.R
+++ section5.R
@@ -14,7 +14,7 @@ setwd("C:\\Users\\Nhi\\Documents\\R-exercises")
 # #for Mac
 # setwd("/Users/Nhi/Desktop/...")
 
-stats <- read.csv('~/R-exercises/dataset/P2-Demographic-Data.csv')
+stats <- read.csv('~/R-exercises/dataset/P2-Demographic-Data.csv',stringsAsFactors = T)
 stats
 
 ##########################################
@@ -32,3 +32,54 @@ summary(stats)
 
 #########################################
 ## Working within data frame with $ 
+head(stats,n=5)
+#Locating Country Angola Birth rate
+stats[3,3]
+stats["Angola",3] #Gives NA
+#In dataframe, there is name for columns, but no names for rows
+#Thus we have to utilize the $ sign to navigate through rows
+
+#Locating column Internet users in 2 ways:
+stats$Internet.users
+stats[,"Internet.users"]
+
+#Locate 2nd row in the column Internet users:
+stats$Internet.users[2]
+
+#Fixing NULL problem when assigning levels
+# stats$Income.Group<- as.factor(stats$Income.Group)
+## Or insert option stringsAsFactors = T when read.csv()
+levels(stats$Income.Group)
+
+########################################################
+## Basic Operations with Dataframe
+
+### Subsetting
+stats[1:10,] #first 10 rows, all columns
+stats[3:9,]
+stats[c(4,100),]#row 4th and 100th, all columns
+
+### Dimensions
+### [] in dataframe does not change into vector with rows like in matrix
+stats[1,]
+is.data.frame(stats[1,]) #data.frame
+
+### HOWEVER with columns:
+stats[,1]
+is.data.frame(stats[,1]) #NOT data.frame
+stats[,1,drop=F] #use drop=F to preserve data.frame
+
+### Math operations within columns (vectorized column)
+head(stats)
+stats$Birth.rate * stats$Internet.users
+stats$Birth.rate + stats$Internet.users
+
+### Adding column
+head(stats)
+stats$MyCalc <- stats$Birth.rate * stats$Internet.users
+
+stats$xyz <- 1:5 #This insufficient length vector produce a recycled vector throughout all columns
+
+### Removing column (similar to name remove)
+stats$MyCalc <- NULL
+stats$xyz <- NULL
