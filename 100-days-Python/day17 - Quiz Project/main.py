38b0dfcb209a4b58813a87187e57b14322dafb09
Nhi Luu <98994882+nluu1@users.noreply.github.com>
2022-06-23 20:55:37 -0400

    in process

---
+++ 100-days-Python/day17 - Quiz Project/main.py
@@ -0,0 +1,7 @@
+# Creating a class
+# Class is a blueprint that we use o create our own object
+class User:
+    pass #to pass the declaration out of error
+
+#Name of Class have to be in PascalCase
+user_1 = User()
